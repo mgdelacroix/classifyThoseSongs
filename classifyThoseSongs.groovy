de8973aed807baf0c404fd73388504d0b65feae8
Miguel de la Cruz <mgdelacroix@gmail.com>
2013-05-19 20:09:18 +0200

    ASCII art added as well as error checking in the directories

--- classifyThoseSongs.groovy
+++ classifyThoseSongs.groovy
@@ -2,7 +2,27 @@
 @Grab('javazoom:jlayer:1.0.1')
 import javazoom.jl.player.Player
 
+println '''
+         .                          ,__           _______ _                             _____                            
+   ___   |     ___    ____   ____ ` /  ` ,    .  '   /    /        __.    ____   ___   (        __.  , __     ___.   ____
+ .'   `  |    /   `  (      (     | |__  |    `      |    |,---. .'   \\  (     .'   `   `--.  .'   \\ |'  `. .'   `  (    
+ |       |   |    |  `--.   `--.  | |    |    |      |    |'   ` |    |  `--.  |----'      |  |    | |    | |    |  `--. 
+  `._.' /\\__ `.__/| \\___.' \\___.' / |     `---|.     /    /    |  `._.' \\___.' `.___, \\___.'   `._.' /    |  `---| \\___.'
+                                    /     \\___/                                                              \\___/       
+'''
+
 if (args) {
+
+    def error = false
+    args.each { dirName ->
+
+        def dir = new File(dirName)
+
+        if (!dir.exists()) {
+            error = true
+        }
+
+    }
 
     def song = new File('08 - Get lucky.mp3')
     new Player(song.newInputStream()).play()
